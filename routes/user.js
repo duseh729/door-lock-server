342f4bb522ca9c2dd11679e112d47818729e7fc9
Doyeon <duseh729@naver.com>
2023-10-05 15:54:59 +0900

    fix: json 데이터 안받아지는 오류 해결

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors"); // cors 라이브러리 import
-const bodyParser = require("body-parser"); // body-parser 라이브러리 import
 
 const { User } = require("../models/user");
 const mongoConnect = require("../util/database").mongoConnect; // mongodb 데이터베이스
 
 router.use(cors()); // cors 사용
-router.use(bodyParser.json());
+router.use(express.json());
 
 router.post("/login", (req, res, next) => {
   const { userId, userPw } = { ...req.body };
   console.log(userId, userPw);
 
   User.findById({ userId: userId })
-    .then((user) => {
+    .then(user => {
       if (!user || user.password !== userPw) {
         return res.status(401).json({ message: "Invalid username or password." });
       }
       res.status(200).json({ message: "Login successful.", userId: userId });
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Error during login:", error);
       res.status(500).json({ message: "Login failed." });
     });
@@ -36,7 +35,7 @@ router.post("/signup", (req, res, next) => {
   }
 
   User.findById({ userId: userId })
-    .then((existingUser) => {
+    .then(existingUser => {
       if (existingUser) {
         return res.json({ message: "이미 존재하는 아이디입니다." });
       }
@@ -45,16 +44,16 @@ router.post("/signup", (req, res, next) => {
 
       user
         .save()
-        .then((result) => {
+        .then(result => {
           console.log("User saved:", result);
           res.status(201).json({ message: "회원가입이 완료되었습니다.", success: true });
         })
-        .catch((error) => {
+        .catch(error => {
           console.error("Error saving user:", error);
           res.status(500).json({ message: "회원가입 중 오류가 발생했습니다." });
         });
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Error checking user:", error);
       res.status(500).json({ message: "회원가입 중 오류가 발생했습니다." });
     });
@@ -62,7 +61,7 @@ router.post("/signup", (req, res, next) => {
 
 router.post("/force", (req, res, next) => {
   const data = { userId: req.body.userId, forceData: req.body.forceData };
-  User.updataOneById(data).then((result) => {
+  User.updataOneById(data).then(result => {
     res.json({ message: "무적 변경" });
   });
 });
